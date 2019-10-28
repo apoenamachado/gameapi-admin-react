dd43fdc200b055e2bbc7adcfe4e7b3c9c44286a9
Apoena Machado <apoenacunha@gmail.com>
2019-10-28 02:41:12 -0300

    Updates

--- precache-manifest.63df7702b44ee6c46142056d967b083a.js
+++ precache-manifest.63df7702b44ee6c46142056d967b083a.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/gameapi-admin-react/static/media/outline-icons.cd6c777f.woff2"
   },
   {
-    "revision": "d298f7c20e3b7c85495d",
+    "revision": "12d7b3f2577258ab8279",
     "url": "/gameapi-admin-react/static/css/main.9e6aa513.chunk.css"
   },
   {
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/gameapi-admin-react/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "60a6c7268dc4295e20f0",
-    "url": "/gameapi-admin-react/static/js/2.6f8802a1.chunk.js"
-  },
-  {
-    "revision": "0ab54153eeeca0ce03978cc463b257f7",
-    "url": "/gameapi-admin-react/static/media/icons.0ab54153.woff2"
+    "revision": "0f9c9253d0ba301b6226",
+    "url": "/gameapi-admin-react/static/js/2.34969c48.chunk.js"
   },
   {
     "revision": "9c74e172f87984c48ddf5c8108cabe67",
     "url": "/gameapi-admin-react/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/gameapi-admin-react/static/media/icons.0ab54153.woff2"
   },
   {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
@@ -44,8 +44,8 @@ self.__precacheManifest = [
     "url": "/gameapi-admin-react/static/media/outline-icons.701ae6ab.eot"
   },
   {
-    "revision": "d298f7c20e3b7c85495d",
-    "url": "/gameapi-admin-react/static/js/main.995e039b.chunk.js"
+    "revision": "12d7b3f2577258ab8279",
+    "url": "/gameapi-admin-react/static/js/main.85bd571e.chunk.js"
   },
   {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
@@ -56,16 +56,16 @@ self.__precacheManifest = [
     "url": "/gameapi-admin-react/static/media/outline-icons.ad97afd3.ttf"
   },
   {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/gameapi-admin-react/static/media/icons.962a1bf3.svg"
+  },
+  {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
     "url": "/gameapi-admin-react/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/gameapi-admin-react/static/media/brand-icons.13db00b7.eot"
-  },
-  {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/gameapi-admin-react/static/media/icons.962a1bf3.svg"
   },
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
@@ -80,11 +80,11 @@ self.__precacheManifest = [
     "url": "/gameapi-admin-react/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
-    "revision": "60a6c7268dc4295e20f0",
-    "url": "/gameapi-admin-react/static/css/2.d7817b0b.chunk.css"
+    "revision": "0f9c9253d0ba301b6226",
+    "url": "/gameapi-admin-react/static/css/2.dae7b94d.chunk.css"
   },
   {
-    "revision": "94cfb178cf8ffa93fb0d7f8b9e7f5945",
+    "revision": "9b6112fcff26878fa9c9556a87cf6355",
     "url": "/gameapi-admin-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gameapi-admin-react/precache-manifest.ae6cb14f4a8bc84739fda4acaacb3de1.js"
+  "/gameapi-admin-react/precache-manifest.63df7702b44ee6c46142056d967b083a.js"
 );
 
 workbox.clientsClaim();
